12704a54b88dc413763b740b128a8de2a83cf558
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-05-25 17:28:55 +0900

    :up:2021

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
